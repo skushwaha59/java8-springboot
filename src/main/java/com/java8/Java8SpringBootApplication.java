43f6bc5330ffc91839c1d6668d9e99e1978d6b36
skushwaha59 <topgun@localhost.localdomain>
2024-04-03 19:37:07 +0530

    First Commit

---
+++ src/main/java/com/java8/Java8SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.java8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8SpringBootApplication.class, args);
+	}
+
+}
